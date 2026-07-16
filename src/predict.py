848d707015c86cca3b64f516756b77ba4783f029
Anup Tripathi <at8925923@gmail.com>
2026-07-16 22:59:00 +0530

    Load model from Hugging Face

--- src/predict.py
+++ src/predict.py
@@ -1,14 +1,27 @@
+import os
 import torch
 from torchvision import transforms
 from transformers import BertTokenizer
+from huggingface_hub import hf_hub_download
 
 from src.models.fusion_model import FusionModel
 from src.config import Config
 
 config = Config("configs/config.yaml")
 
+# -------------------------------
+# Download model from Hugging Face
+# -------------------------------
+MODEL_PATH = hf_hub_download(
+    repo_id="anuptripathi5592/multimodal-healthcare-ai",
+    filename="model.pth"
+)
+
+# -------------------------------
+# Load Model
+# -------------------------------
 model = FusionModel(config)
-model.load_state_dict(torch.load("artifacts/model.pth", map_location="cpu"))
+model.load_state_dict(torch.load(MODEL_PATH, map_location="cpu"))
 model.eval()
 
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
@@ -32,6 +45,7 @@ classes = [
 
 
 def predict(image, text):
+
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor()
